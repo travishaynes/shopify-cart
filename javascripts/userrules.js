fb14c928cad945f03b065a10c73ba9decc4dc877
Travis Haynes <travis.j.haynes@gmail.com>
2011-12-12 19:13:41 -0800

    added feedback badge; added header and left-nav

---
+++ javascripts/userrules.js
@@ -0,0 +1,18 @@
+var _usrp = {
+	type                : "button",
+	company             : "travishaynes",
+	host                : "travishaynes.userrules.com",
+	mode                : "advanced",
+	sso                 : {},
+	getVisitorId        : function() {return "";},
+	getAdditionalParams : function() {return "";},
+	placement           : "right",
+	color               : ["#f37500", "#f37500"]
+};
+
+(function(D) {
+	var _usr = D.createElement("script"), s = D.getElementsByTagName("script")[0];
+	_usr.type = "text/javascript"; _usr.async = true;
+	_usr.src = ("https:" == D.location.protocol ? "https" : "http" ) + "://dtkm4pd19nw6z.cloudfront.net/js/userrules/9a41a826e957127b4e5bfc7d140e45c4/feedback.canary.js";
+	s.parentNode.insertBefore(_usr, s);
+})(document);
